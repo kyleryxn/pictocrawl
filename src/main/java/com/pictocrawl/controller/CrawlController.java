9a7405dc01fe3a1ca8a2b23b092f483c140ee1d9
Kyle <kyle.schoenhardt@mail.citytech.cuny.edu>
2023-08-09 18:35:41 -0400

    [feat] Add variable images in CrawlController
    This commit introduces the addition of variable `images` in the `CrawlController` class. The changes involve implementing logic to handle images dynamically during the crawling process.
    
    By adding variable `images`, the `CrawlController` can now process and manage images in a more flexible and adaptable manner. This enhancement contributes to the overall functionality of the crawling process, allowing the controller to interact with and manipulate images based on the specific requirements of the application.
    
    The introduction of variable `images` aligns with the project's goal of enhancing image handling and management, ultimately improving the user experience and content presentation.

--- src/main/java/com/pictocrawl/controller/CrawlController.java
+++ src/main/java/com/pictocrawl/controller/CrawlController.java
@@ -1,13 +1,19 @@
 package com.pictocrawl.controller;
 
+import com.pictocrawl.model.image.Image;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Set;
+
 @Controller
 public class CrawlController {
+    private Set<Image> images;
 
     @GetMapping("/crawl")
     public String crawl() {
+
+
         return "crawl";
     }
 
