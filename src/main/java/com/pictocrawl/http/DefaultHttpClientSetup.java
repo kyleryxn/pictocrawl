dc5b7bc310c28c7aef91bcf74b83b96eaf4b571e
Kyle <kyle.schoenhardt@mail.citytech.cuny.edu>
2024-02-08 20:05:55 -0500

    Small updates

--- src/main/java/com/pictocrawl/http/DefaultHttpClientSetup.java
+++ src/main/java/com/pictocrawl/http/DefaultHttpClientSetup.java
@@ -9,7 +9,6 @@ import org.apache.hc.core5.http.io.SocketConfig;
 import org.apache.hc.core5.http2.HttpVersionPolicy;
 import org.apache.hc.core5.pool.PoolConcurrencyPolicy;
 import org.apache.hc.core5.pool.PoolReusePolicy;
-import org.apache.hc.core5.pool.PoolStats;
 import org.apache.hc.core5.util.Timeout;
 
 public class DefaultHttpClientSetup implements HttpClientSetup {
@@ -37,21 +36,21 @@ public class DefaultHttpClientSetup implements HttpClientSetup {
         return socketConfig;
     }
 
-    public PoolingHttpClientConnectionManager getConnManager() {
-        return connManager;
-    }
-
     private void setConnManager() {
         this.connManager = PoolingHttpClientConnectionManagerBuilder.create()
                 .setDefaultSocketConfig(socketConfig)
                 .setConnPoolPolicy(PoolReusePolicy.FIFO)
                 .setPoolConcurrencyPolicy(PoolConcurrencyPolicy.STRICT)
                 .setDefaultTlsConfig(TlsConfig.custom()
-                        .setVersionPolicy(HttpVersionPolicy.FORCE_HTTP_2)
+                        .setVersionPolicy(HttpVersionPolicy.NEGOTIATE)
                         .build())
                 .setMaxConnTotal(maxConnTotal)
                 .setMaxConnPerRoute(maxConnPerRoute)
                 .build();
+    }
+
+    public PoolingHttpClientConnectionManager getConnManager() {
+        return connManager;
     }
 
     @Override
